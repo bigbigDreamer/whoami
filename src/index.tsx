8c2662d382fd616260ff9dee951c16ee85e1a3e7
eric_wang <jadeface.scholar@gmail.com>
2022-07-26 21:23:20 +0800

    feat: access sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import Launcher from '@aktiv/launcher';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import RouterConfig from './config/router.config';
 
 const app = new Launcher({
     routes: RouterConfig,
+});
+
+Sentry.init({
+    dsn: 'https://c31e50ccade94d8cae4608282e23846d@o1335044.ingest.sentry.io/6602466',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 });
 
 app.start();
